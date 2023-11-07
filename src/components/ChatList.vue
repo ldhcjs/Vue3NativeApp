f9a045e3c4d85a8cd8a47ebc112079202a4f93bc
TonyLee <tony.lee1@cj.net>
2023-11-07 18:31:00 +0900

    [Tony] Added ActionBar in ChatList.

--- src/components/ChatList.vue
+++ src/components/ChatList.vue
@@ -1,12 +1,25 @@
 <template>
     <Page>
+        <ActionBar>
+            <GridLayout columns="40, *">
+                <Label col="1" text="ChatList View" fontSize="20" />
+                <Label
+                    col="0"
+                    class="fas"
+                    text="&#xf060;"
+                    @tap="$navigateBack"
+                    fontSize="20"
+                />
+            </GridLayout>
+        </ActionBar>
+
         <GridLayout rows="*, auto">
-            <ListView :items="msgs" row="0">
-                <template v-slot:item="{ item }">
-                    <GridLayout rows="auto">
-                        <Label :text="item.text" />
-                        <Label :text="item.sender" />
-                        <Label :text="item.time" />
+            <ListView for="item in itemList" :items="msgs" row="0">
+                <template v-slot:item="item">
+                    <GridLayout rows="auto, auto, auto" columns="*, auto">
+                        <Label text="item.text" row="0" />
+                        <Label text="item.sender" row="1" />
+                        <Label text="item.time" row="2" />
                     </GridLayout>
                 </template>
             </ListView>
@@ -36,11 +49,6 @@ export default {
                 text: "Hello Hyun",
                 sender: "Tony",
                 time: "14:30",
-            },
-            {
-                text: "Hello Tony",
-                sender: "Hyun",
-                time: "14:40",
             },
         ]);
         const sendMsg = () => {
