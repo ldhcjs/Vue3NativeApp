5507214cc4efc329ad9a0fc9e68b77a74dd373f0
TonyLee <tony.lee1@cj.net>
2023-11-13 18:32:36 +0900

    [Tony] Implementing SQLite DB (ing)

--- src/components/DatabaseExam.vue
+++ src/components/DatabaseExam.vue
@@ -1,71 +1,119 @@
-<template></template>
+<template>
+    <Page>
+        <ActionBar>
+            <GridLayout columns="40, *">
+                <Label col="1" text="Database Exam" fontSize="20" />
+                <Label
+                    col="0"
+                    class="fas"
+                    text="&#xf060;"
+                    @tap="$navigateBack"
+                    fontSize="20"
+                />
+            </GridLayout>
+        </ActionBar>
+
+        <GridLayout rows="auto,auto,*">
+            <Button
+                row="0"
+                @tap="onInsert(datas)"
+                class="mt-4 px-4 py-2 bg-white border-2 border-blue-400 rounded-lg"
+            >
+                Insert Data
+            </Button>
+            <Button
+                row="1"
+                @tap="onSelect"
+                class="mt-4 px-4 py-2 bg-white border-2 border-blue-400 rounded-lg"
+            >
+                Select Data
+            </Button>
+            <Label row="2" :text="datas" textWrap="true" whiteSpace="normal" />
+        </GridLayout>
+    </Page>
+</template>
 
 <script lang="ts">
-import { ref, $navigateBack } from "nativescript-vue";
-// import { Sqlite } from "nativescript-sqlite";
-import { Observable } from "@nativescript/core/data/observable";
-import {
-    SQLiteDatabase,
-    openOrCreate,
-    deleteDatabase,
-} from "@nativescript-community/sqlite";
+import { ref, $navigateBack, onMounted } from "nativescript-vue";
+import { openOrCreate } from "@nativescript-community/sqlite";
 import { knownFolders, path } from "@nativescript/core/file-system";
-import { ImageSource } from "@nativescript/core";
-import {
-    BaseEntity,
-    Column,
-    Connection,
-    Entity,
-    PrimaryGeneratedColumn,
-    createConnection,
-} from "@nativescript-community/typeorm/browser";
-import { installMixins } from "@nativescript-community/sqlite/typeorm";
+import { alert } from "@nativescript/core";
 
-const database = openOrCreate("path/to/db");
 interface DBData {
     id: number;
     name: string;
 }
 
-// const sqlite = openOrCreate("path/to/db");
-// sqlite.execute("CREATE TABLE names (id INT, name TEXT)");
-// sqlite.transaction((cancelTransaction) => {
-//     // Calling cancelTransaction will rollback all changes made to db
-//     names.forEach((name, id) =>
-//         sqlite.execute("INSERT INTO names (id, name) VALUES (?, ?)", [id, name])
-//     );
-// });
-
-// const database = new Sqlite();
-// database.open("mydatabase.db");
+const filePath = path.join(
+    knownFolders.documents().getFolder("db").path,
+    "myDB.db"
+);
+const database = openOrCreate(filePath);
+console.log(`path : ${filePath}`);
 
 export default {
+    onMounted() {
+        const createCmd =
+            "CREATE TABLE names (id INT, name TEXT, json TEXT, PRIMARY KEY (id))";
+        database.execute(createCmd);
+
+        console.log(`onMounted CREATE : ${createCmd}`);
+
+        // generate Data
+        // let i = 0;
+        // const data: DBData[] = [];
+        // while (i < 10) {
+        //     data.push({ id: i, name: `${Math.random() + i} Test data` });
+        //     ++i;
+        // }
+        // console.log("Generated " + i + " data items.");
+    },
     setup() {
-        const users = ref("user");
-
-        const updateUser = (id: number, name: string, age: number) => {
-            // 데이터베이스 업데이트
-            database.execute(
-                "UPDATE users SET name = ?, age = ? WHERE id = ?",
-                [name, age, id]
-            );
-
-            // 데이터 조회
-            users.value = database.query("SELECT * FROM users");
+        const datas = ref<DBData[]>([
+            {
+                id: 0,
+                name: "Tony0",
+            },
+            {
+                id: 1,
+                name: "Tony1",
+            },
+        ]);
+        const onSelect = () => {
+            const select = "SELECT * FROM names WHERE id < 20";
+            const result = database.select(select);
+            // result가 배열인지 확인 후 처리
+            if (Array.isArray(result)) {
+                result.forEach((item) => {
+                    datas.value.push({
+                        id: item.id,
+                        name: item.name,
+                    });
+                });
+                alert(`Received data: ${JSON.stringify(result)}`);
+            } else {
+                // result가 배열이 아니면 예외 처리 또는 다른 로직 추가
+                console.error("Result is not an array");
+            }
+            alert(`Received data: ${JSON.stringify(result)}`);
         };
+        async function onInsert(data: DBData[]) {
+            for (let index = 0; index < 10; index++) {
+                const d = data[index];
+                const insert =
+                    "INSERT INTO names (id, name, json) VALUES (?, ?, ?)";
+                await database.execute(insert);
+            }
 
-        const deleteUser = (id: number) => {
-            // 데이터베이스 삭제
-            database.execute("DELETE FROM users WHERE id = ?", [id]);
-
-            // 데이터 조회
-            users.value = database.query("SELECT * FROM users");
-        };
-
+            console.log(`data : ${data}`);
+        }
         return {
+            datas,
             database,
-            updateUser,
-            deleteUser,
+            //updateUser,
+            //deleteUser,
+            onSelect,
+            onInsert,
         };
     },
 };
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,7 +6,7 @@
             </ActionBar>
 
             <GridLayout
-                rows="*, auto, auto, auto, auto, auto, auto, *"
+                rows="*, auto, auto, auto, auto, auto, auto, auto, *"
                 class="px-4"
             >
                 <Label
@@ -60,6 +60,15 @@
                 >
                     ChatList
                 </Button>
+
+                <Button
+                    row="7"
+                    @tap="$navigateTo(DatabaseExam)"
+                    class="mt-4 px-4 py-2 bg-white border-2 border-blue-400 rounded-lg"
+                    horizontalAlignment="center"
+                >
+                    Database Exam
+                </Button>
             </GridLayout>
         </Page>
     </Frame>
@@ -78,6 +87,7 @@ import BasicDrawer from "./BasicDrawer.vue";
 import BottomTabNavigation from "@/components/BottomTabNavigation.vue";
 import NetworkTest from "@/components/NetworkTest.vue";
 import ChatList from "@/components/ChatList.vue";
+import DatabaseExam from "@/components/DatabaseExam.vue";
 
 const counter = ref(0);
 const message = computed(() => {
